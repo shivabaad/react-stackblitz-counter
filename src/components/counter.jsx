dbf295b239cad19bb34c9f4e892bbd3509723bdd
shivabaad <shivaprasadbaad@gmail.com>
2022-03-31 21:44:28 +0530

    2 files modified

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,72 +3,75 @@ import ReactDOM from 'react-dom';
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.value,
   };
 
-  styles = {}
+  styles = {};
 
   render() {
-
-    console.log('props', this.props)
+    console.log('props', this.props.children);
     return (
       <React.Fragment>
+        {this.props.children}
         <h3>Counter</h3>
         <span className={this.getbadgeClass()}>{this.formatCount()}</span>
         <button onClick={this.handleIncrement}>Increment</button> {/* 3 */}
         <button onClick={this.handleDecrement}>Decrement</button> {/* 4 */}
-        <button onClick={this.doHandleIncrement}>Increment</button> {/* 5 and 6 */}
-        <button onClick={() => this.handleIncrementArgArrow(1)}>Increment</button> {/* 7 */}
+        <button onClick={this.doHandleIncrement}>Increment</button>{' '}
+        {/* 5 and 6 */}
+        <button onClick={() => this.handleIncrementArgArrow(1)}>
+          Increment
+        </button>{' '}
+        {/* 7 */}
       </React.Fragment>
     );
   }
-  
-  // 1 
+
+  // 1
   formatCount = () => {
-    const {value} = this.state;
+    const { value } = this.state;
     return value === 0 ? 'Zero' : value;
-  }
-  
-  // 2  
+  };
+
+  // 2
   getbadgeClass = () => {
     let badge = 'badge m-2 badge-';
-    badge += this.state.value === 0 ? 'warning':'primary';
-    console.log(badge)
+    badge += this.state.value === 0 ? 'warning' : 'primary';
+    console.log(badge);
     return badge;
-  }
+  };
 
   // 3
   handleIncrement = () => {
-    this.setState({value: this.state.value+1})
+    this.setState({ value: this.state.value + 1 });
     console.log('Increment clicked', this.state.value);
-  }
-  
+  };
+
   // 4
   handleDecrement = () => {
-    if(this.state.value <= 0){
+    if (this.state.value <= 0) {
       return 'disabled';
     }
-    this.setState({value: this.state.value-1})
+    this.setState({ value: this.state.value - 1 });
     console.log('Decrement clicked', this.state.value);
-  }
-  
+  };
+
   // 5    Passing event arguments
   handleIncrementArg = (product) => {
-    console.log(product)
-    this.setState({value: this.state.value+1})
-  }
-  
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
   // 6  Linked to 5
   doHandleIncrement = () => {
-    this.handleIncrementArg({id:1})
-  }
-  
+    this.handleIncrementArg({ id: 1 });
+  };
+
   // 7    Passing event arguments using arrow
   handleIncrementArgArrow = (product) => {
-    console.log(product)
-    this.setState({value: this.state.value+1})
-  }
-    
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,9 +5,9 @@ import Counter from './counter';
 class Counters extends Component {
   state = {
     counters: [
-      { key: 1, value: 1 },
-      { key: 2, value: 2 },
-      { key: 3, value: 3 },
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
     ],
   };
 
@@ -15,7 +15,9 @@ class Counters extends Component {
     return (
       <React.Fragment>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.key} value={counter.value} />
+          <Counter key={counter.id} value={counter.value}>
+            <div>Title {counter.id}</div>
+          </Counter>
         ))}
       </React.Fragment>
     );
